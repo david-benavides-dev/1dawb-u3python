24c5af8bd981f4fc74fd2ac22958d138ac93dd66
dbenfon341 <dbenfon341@g.educaand.es>
2024-11-30 22:24:29 +0100

    typo

--- src/cadenas/ej30_03.py
+++ src/cadenas/ej30_03.py
@@ -1,13 +1,11 @@
 # Ejercicio 3.0.3
 # Tienes este código:
-
 # palabra = 'banana'
 # contador = 0
 # for letra in palabra:
 #     if letra == 'a':
 #         contador = contador + 1
 # print(contador)
-
 # Encapsúlalo en una función llamada cuenta, y hazla genérica de tal modo que pueda aceptar una cadena y una letra como argumentos.
 
 
